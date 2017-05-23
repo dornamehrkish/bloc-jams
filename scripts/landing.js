a588d1580bfaa643ee2a9dd488ba9feddef076eb
Dorna Mehrkish <dmehrkish91@gmail.com>
2017-05-23 16:19:20 -0700

    Assignment 21 completed, attemped refactored solution

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,12 @@
-var animatePoints = function() {
+var revealPoint = function() {
 
     var points = document.getElementsByClassName('point');
+    for (var i = 0; i < 3 i++) {
 
-    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+    points.style.opacity = 1;
+    points.style.transform = "scaleX(.5) translateX(0)";
+    points.style.msTransform = "scaleX(.5) translateY(0)";
+    points.style.WebkitTransform = "scaleX(.5) translateX(0)";
     };
-
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
 
 };
