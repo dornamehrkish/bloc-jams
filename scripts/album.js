e827c8d4ca84605f3dd4d95a64049d602aeca80b
Dorna Mehrkish <dmehrkish91@gmail.com>
2017-05-26 13:59:42 -0700

    Assignment 24 Album View

--- scripts/album.js
+++ scripts/album.js
@@ -29,6 +29,21 @@ var albumMarconi = {
      ]
  };
 
+ var albumFleetFoxes = {
+     title: 'Sun Giant',
+     artist: 'Fleet Foxes',
+     label: 'Sub Pop Records',
+     year: '2008',
+     albumArtUrl: 'assets/images/album_covers/fleet.png',
+     songs: [
+         { title: 'Sun Giant', duration: '2:14' },
+         { title: 'Drops in the River', duration: '4:13' },
+         { title: 'English House', duration: '4:41' },
+         { title: 'Mykonos', duration: '4:35'},
+         { title: 'Innocent Son', duration: '3:07'}
+     ]
+ };
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -40,30 +55,43 @@ var albumMarconi = {
 
      return template;
  };
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 
  var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-     // #2
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
 
-     // #3
+
      albumSongList.innerHTML = '';
 
-     // #4
+  
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
 
- window.onload = function() {
-     setCurrentAlbum(albumPicasso);
- };
+
+
+window.onload = function(){
+  setCurrentAlbum(albumPicasso);
+
+  var albums = [albumPicasso, albumMarconi, albumFleetFoxes];
+  var index = 1;
+  albumImage.addEventListener("click", function(event) {
+    setCurrentAlbum(albums[index]);
+    index++;
+    if (index == albums.length) {
+      index = 0;
+    }
+
+});
+
+};
