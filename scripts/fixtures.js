884b813c8d59b4ade6cc5a83789de08da8f3fc28
Dorna Mehrkish <dmehrkish91@gmail.com>
2017-06-07 12:54:46 -0700

    Checkpoint 31 next and previous buttons: completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,45 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
+
+
+var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ var albumFleetFoxes = {
+     title: 'Sun Giant',
+     artist: 'Fleet Foxes',
+     label: 'Sub Pop Records',
+     year: '2008',
+     albumArtUrl: 'assets/images/album_covers/fleet.png',
+     songs: [
+         { title: 'Sun Giant', duration: '2:14' },
+         { title: 'Drops in the River', duration: '4:13' },
+         { title: 'English House', duration: '4:41' },
+         { title: 'Mykonos', duration: '4:35'},
+         { title: 'Innocent Son', duration: '3:07'}
+     ]
+ };
